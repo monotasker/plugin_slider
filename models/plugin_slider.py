ec2b3a6f7f50ed5f0e3874a4fb0a8a787eac2c44
Ian W. Scott <iscott@tyndale.ca>
2013-05-27 17:43:53 -0400

    committing submodule changes

--- models/plugin_slider.py
+++ models/plugin_slider.py
@@ -12,18 +12,18 @@ response.files.append(URL('static', 'plugin_ajaxselect/plugin_ajaxselect.css'))
 response.files.append(URL('static', 'plugin_slider/plugin_slider.css'))
 
 db.define_table('plugin_slider_themes',
-    Field('theme_name', 'string'),
-    Field('description', 'text'),
-    format='%(theme_name)s'
-    )
+                Field('theme_name', 'string'),
+                Field('description', 'text'),
+                format='%(theme_name)s'
+                )
 
 db.define_table('plugin_slider_slides',
-    Field('slide_name', 'string'),
-    Field('content', 'text'),
-    Field('theme', 'list:reference plugin_slider_themes'),
-    Field('updated', 'datetime', default=datetime.datetime.utcnow()),
-    format='%(slide_name)s'
-    )
+                Field('slide_name', 'string'),
+                Field('content', 'text'),
+                Field('theme', 'list:reference plugin_slider_themes'),
+                Field('updated', 'datetime', default=datetime.datetime.utcnow()),
+                format='%(slide_name)s'
+                )
 db.plugin_slider_slides.theme.requires = IS_IN_DB(db,
                                     'plugin_slider_themes.id',
                                     db.plugin_slider_themes._format,
@@ -35,12 +35,12 @@ db.plugin_slider_slides.content.widget = lambda field, value: \
                                                         'image_picker.load')
 
 db.define_table('plugin_slider_decks',
-    Field('deck_name', 'string'),
-    Field('deck_slides', 'list:reference plugin_slider_slides'),
-    Field('theme', 'list:reference plugin_slider_themes'),
-    Field('position', 'integer'),
-    format='%(deck_name)s'
-    )
+                Field('deck_name', 'string'),
+                Field('deck_slides', 'list:reference plugin_slider_slides'),
+                Field('theme', 'list:reference plugin_slider_themes'),
+                Field('position', 'integer'),
+                format='%(deck_name)s'
+                )
 db.plugin_slider_decks.deck_slides.requires = IS_IN_DB(db,
                                     'plugin_slider_slides.id',
                                     db.plugin_slider_slides._format,
@@ -50,12 +50,13 @@ db.plugin_slider_decks.theme.requires = IS_IN_DB(db,
                                     db.plugin_slider_themes._format,
                                     multiple=True)
 db.plugin_slider_decks.deck_slides.widget = lambda field, value: \
-                                    AjaxSelect().widget(
-                                        field, value, 'plugin_slider_slides',
-                                        refresher=True,
-                                        multi='basic',
-                                        lister='editlinks',
-                                        sortable='true')
+                                    AjaxSelect(field, value,
+                                               'plugin_slider_slides',
+                                               refresher=True,
+                                               multi='basic',
+                                               lister='editlinks',
+                                               sortable='true'
+                                               ).widget()
 
 
 class Plugin_slider_decksVirtualFields(object):
