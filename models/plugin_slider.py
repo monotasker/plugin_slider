d0e33e74d68ca45c0022ad5fead729d58384927c
Ian W. Scott <iscott@tyndale.ca>
2013-07-29 15:37:50 -0400

    updated .gitignore to catch *.un~; changed model to avoid reserved field label

--- models/plugin_slider.py
+++ models/plugin_slider.py
@@ -19,7 +19,7 @@ db.define_table('plugin_slider_themes',
 
 db.define_table('plugin_slider_slides',
                 Field('slide_name', 'string'),
-                Field('content', 'text'),
+                Field('slide_content', 'text'),
                 Field('theme', 'list:reference plugin_slider_themes'),
                 Field('updated', 'datetime', default=datetime.datetime.utcnow()),
                 format='%(slide_name)s'
@@ -28,7 +28,7 @@ db.plugin_slider_slides.theme.requires = IS_IN_DB(db,
                                     'plugin_slider_themes.id',
                                     db.plugin_slider_themes._format,
                                     multiple=True)
-db.plugin_slider_slides.content.widget = lambda field, value: \
+db.plugin_slider_slides.slide_content.widget = lambda field, value: \
                                         JQMODAL(field, value).textarea('image',
                                                         'image_picker',
                                                         'plugin_widgets',
@@ -38,7 +38,7 @@ db.define_table('plugin_slider_decks',
                 Field('deck_name', 'string'),
                 Field('deck_slides', 'list:reference plugin_slider_slides'),
                 Field('theme', 'list:reference plugin_slider_themes'),
-                Field('position', 'integer'),
+                Field('deck_position', 'integer'),
                 format='%(deck_name)s'
                 )
 db.plugin_slider_decks.deck_slides.requires = IS_IN_DB(db,
